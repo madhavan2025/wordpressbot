2d4e600866c8f5233f7bb7634861f26d65d39425
madhavan2025 <madhavan.sykasys@gmail.com>
2026-03-04 17:08:15 +0530

    Initial commit-maddyee22

--- public/chatbot.js
+++ public/chatbot.js
@@ -1,22 +1,13 @@
 (function () {
-  // avoid loading twice
-  if (window.__CHATBOT_LOADED__) return;
-  window.__CHATBOT_LOADED__ = true;
-
   var iframe = document.createElement("iframe");
-
-  iframe.src = "https://chatbot11-rouge.vercel.app/"; // your floating chat page
+  iframe.src = "https://sykasysbot.vercel.app/";
   iframe.style.position = "fixed";
   iframe.style.bottom = "20px";
   iframe.style.right = "20px";
   iframe.style.width = "380px";
   iframe.style.height = "600px";
   iframe.style.border = "none";
-  iframe.style.borderRadius = "14px";
-  iframe.style.boxShadow = "0 10px 30px rgba(0,0,0,0.2)";
-  iframe.style.zIndex = "999999";
-
-  iframe.allow = "clipboard-write; microphone";
+  iframe.style.zIndex = "9999";
 
   document.body.appendChild(iframe);
 })();
